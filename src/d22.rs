df847f4315b2e58af650757f5d69542dfdd9ac68
Joe Dane <joe.dane@gmail.com>
2024-10-24 05:44:19 -1000

    D22 WIP

--- src/d22.rs
+++ src/d22.rs
@@ -22,64 +22,110 @@ impl Point {
 }
 
 #[derive(Debug)]
-enum Orientation {
-    X,
-    Y,
-    Z,
-}
-
-#[derive(Debug)]
 struct Obj {
     label: String,
     start: Point,
     end: Point,
 }
 
-struct PointIter {
-    steps: usize,
-    next: Point,
+struct PointIterator {
+    start: Point,
     end: Point,
-    dir: Orientation,
+    seen: u16,
 }
 
-impl Iterator for PointIter {
+impl Iterator for PointIterator {
     type Item = Point;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.next == self.end {
-            None
+        let (start, end) = (self.start, self.end);
+        if start.x != end.x {
+            if start.x + self.seen > end.x {
+                None
+            } else {
+                let r = Some(Point::new(start.x + self.seen, start.y, start.z));
+                self.seen += 1;
+                r
+            }
+        } else if start.y != end.y {
+            if start.y + self.seen > end.y {
+                None
+            } else {
+                let r = Some(Point::new(start.x, start.y + self.seen, start.z));
+                self.seen += 1;
+                r
+            }
+        } else if start.z != end.z {
+            if start.z + self.seen > end.z {
+                None
+            } else {
+                let r = Some(Point::new(start.x, start.y, start.z + self.seen));
+                self.seen += 1;
+                r
+            }
         } else {
-            let next = self.next;
-            self.next = match self.dir {
-                Orientation::X => Point::new(next.x + 1, next.y, next.z),
-                Orientation::Y => Point::new(next.x, next.y + 1, next.z),
-                Orientation::Z => Point::new(next.x, next.y, next.z + 1),
-            };
-            Some(next)
+            if self.seen > 0 {
+                None
+            } else {
+                let r = Some(Point::new(start.x, start.y, start.z));
+                self.seen += 1;
+                r
+            }
         }
     }
 }
 impl Obj {
-    fn get_orientation(&self) -> Orientation {
-        if self.start.x != self.end.x {
-            Orientation::X
-        } else if self.start.y != self.end.y {
-            Orientation::Y
-        } else if self.start.z != self.end.z {
-            Orientation::Z
+    fn new(label: String, start: Point, end: Point) -> Self {
+        if start.x != end.x {
+            if start.x < end.x {
+                Self { label, start, end }
+            } else {
+                Self {
+                    label,
+                    start: end,
+                    end: start,
+                }
+            }
+        } else if start.y != end.y {
+            if start.y < end.y {
+                Self { label, start, end }
+            } else {
+                Self {
+                    label,
+                    start: end,
+                    end: start,
+                }
+            }
+        } else if start.z != end.z {
+            if start.z < end.z {
+                Self { label, start, end }
+            } else {
+                Self {
+                    label,
+                    start: end,
+                    end: start,
+                }
+            }
         } else {
-            panic!()
+            // start and end are the same point
+            Self { label, start, end }
         }
     }
+
     fn points(&self) -> impl Iterator<Item = Point> {
-        PointIter {
-            steps: 0,
-            next: self.start,
+        PointIterator {
+            start: self.start,
             end: self.end,
-            dir: self.get_orientation(),
+            seen: 0,
         }
+    }
+
+    fn drop(&mut self) {
+        self.start.z -= 1;
+        self.end.z -= 1;
     }
 }
+
 impl std::str::FromStr for Obj {
     type Err = &'static str;
 
@@ -106,31 +152,15 @@ impl std::str::FromStr for Obj {
     }
 }
 
-fn display_grid(grid: &Vec<Vec<u16>>) {
-    for i in 0..grid.len() {
-        print!("|");
-        for j in 0..grid[i].len() {
-            print!("{:3}|", grid[i][j])
-        }
-        println!("");
-    }
+#[derive(Debug)]
+struct Grid {
+    max_z: Vec<Vec<u16>>,
+    x_offset: u16,
+    y_offset: u16,
 }
-impl PuzzleRun for Part1 {
-    fn input_data(&self) -> anyhow::Result<&str> {
-        /*
-                Ok("1,0,1~1,2,1
-        0,0,2~2,0,2
-        0,2,3~2,2,3
-        0,0,4~0,2,4
-        2,0,5~2,2,5
-        0,1,6~2,1,6
-        1,1,8~1,1,9")
-                */
-        read_file("input/day22.txt")
-    }
 
-    fn run(&self, input: &str) -> String {
-        let mut objs: Vec<Obj> = input.lines().map(|s| s.parse().unwrap()).collect();
+impl Grid {
+    fn new(objs: &Vec<Obj>) -> Self {
         let Some((ll, ur)) = objs
             .iter()
             .map(|o| {
@@ -166,41 +196,70 @@ impl PuzzleRun for Part1 {
             v
         });
 
-        let mut grounded = max_z.clone();
-        let mut grounded_idx: Vec<usize> = vec![];
-
-        for (idx, obj) in objs.iter().enumerate() {
-            let is_grounded = u16::min(obj.start.z, obj.end.z) == 1;
-            if is_grounded {
-                grounded_idx.push(idx);
-            }
+        for obj in objs.iter() {
             for p in obj.points() {
-                max_z[p.x as usize][p.y as usize] =
-                    u16::max(max_z[p.y as usize][p.x as usize], p.z);
-                if is_grounded {
-                    grounded[p.y as usize][p.x as usize] = p.z;
-                }
+                let (xx, yy) = ((p.x - ll.x) as usize, (p.y - ll.y) as usize);
+                max_z[yy][xx] = u16::max(max_z[yy][xx], p.z);
             }
         }
+
+        Self {
+            max_z,
+            x_offset: ll.x,
+            y_offset: ll.y,
+        }
+    }
+
+    fn get_max(&self, y: u16, x: u16) -> u16 {
+        self.max_z[(y - self.y_offset) as usize][(x - self.x_offset) as usize]
+    }
+
+    fn set_max(&mut self, y: u16, x: u16, val: u16) {
+        self.max_z[(y - self.y_offset) as usize][(x - self.x_offset) as usize] = val;
+    }
+}
+fn display_grid(grid: &Vec<Vec<u16>>) {
+    for i in 0..grid.len() {
+        print!("|");
+        for j in 0..grid[i].len() {
+            print!("{:3}|", grid[i][j])
+        }
+        println!("");
+    }
+}
+impl PuzzleRun for Part1 {
+    fn input_data(&self) -> anyhow::Result<&str> {
+        Ok("1,0,1~1,2,1
+        0,0,2~2,0,2
+        0,2,3~2,2,3
+        0,0,4~0,2,4
+        2,0,5~2,2,5
+        0,1,6~2,1,6
+        1,1,8~1,1,9")
+
+        //        read_file("input/day22.txt")
+    }
+
+    fn run(&self, input: &str) -> String {
+        let mut objs: Vec<Obj> = input.lines().map(|s| s.parse().unwrap()).collect();
+        let mut max_z = Grid::new(&objs);
+        println!("{:?}", max_z);
+        /*
         for idx in grounded_idx {
             objs.swap_remove(idx);
         }
-        objs.sort_by_key(|o| -1 * u16::min(o.start.z, o.end.z) as i32);
-        while let Some(mut o) = objs.pop() {
-            let d = o
-                .points()
-                .map(|p| p.z - max_z[p.y as usize][p.x as usize])
-                .min()
-                .unwrap();
-            println!("obj [{}] falling by {}", o.label, d);
-            o.start.z -= d;
-            o.end.z -= d;
-            for p in o.points() {
-                max_z[p.y as usize][p.x as usize] = u16::min(o.start.z, o.end.z);
+        */
+        objs.sort_by_key(|o| u16::min(o.start.z, o.end.z) as i32);
+        for o in objs.iter_mut() {
+            while o.points().all(|p| p.z > max_z.get_max(p.y, p.x)) {
+                println!("dropping obj [{}]", o.label);
+                o.drop();
             }
+            o.points()
+                .for_each(|p| max_z.set_max(p.y, p.x, u16::max(p.z, max_z.get_max(p.y, p.x))));
         }
 
-        format!("ll: {:?}, ur: {:?}", ll, ur)
+        "FOO".to_string()
     }
 }
 
@@ -214,6 +273,28 @@ mod test {
     #[test]
     fn test_part1() {
         println!("{}", Part1.run(Part1.input_data().unwrap()));
+    }
+
+    #[test]
+    fn test_point_iter() {
+        let o = Obj::new(
+            "test".to_string(),
+            Point::new(5, 5, 5),
+            Point::new(10, 5, 5),
+        );
+        let mut it = o.points();
+        assert_eq!(it.next(), Some(Point::new(5, 5, 5)));
+        assert_eq!(it.next(), Some(Point::new(6, 5, 5)));
+        assert_eq!(it.next(), Some(Point::new(7, 5, 5)));
+        assert_eq!(it.next(), Some(Point::new(8, 5, 5)));
+        assert_eq!(it.next(), Some(Point::new(9, 5, 5)));
+        assert_eq!(it.next(), Some(Point::new(10, 5, 5)));
+        assert_eq!(it.next(), None);
+
+        let o = Obj::new("test".to_string(), Point::new(5, 5, 5), Point::new(5, 5, 5));
+        let mut it = o.points();
+        assert_eq!(it.next(), Some(Point::new(5, 5, 5)));
+        assert_eq!(it.next(), None);
     }
 
     #[test]
